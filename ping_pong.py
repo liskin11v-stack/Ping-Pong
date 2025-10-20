760cf65a66689d7f209509188ccd313e18f26025
prog-vlad <liskin11v@gmail.com>
2025-10-20 20:52:18 +0300

    Update ping_pong.py
    Обновление:
    - класс Мяч;
    - отскоки мяча от ракеток и стен;
    - delete Herobrine

--- ping_pong.py
+++ ping_pong.py
@@ -27,19 +27,32 @@ class Player(GameSprite):
         if keys[K_DOWN] and self.rect.y < 600 - 200:
             self.rect.y += self.speed_y
 
+class Ball(GameSprite):
+    def update(self, rocketL, rocketR):
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+        if sprite.collide_rect(self, rocketL) or sprite.collide_rect(self, rocketR):
+            self.speed_x *= -1
+        if self.rect.y < 0 or self.rect.y > 600 - 70:
+            self.speed_y *= -1
+
 window = display.set_mode((600, 600))
 clock = time.Clock()
 
 player_L = Player("left.png", 0, 5, 5, 200, 40, 200)
 player_R = Player("right.png", 0, 5, 555, 200, 40, 200)
+ball = Ball("ball.png", 3, 3, 265, 265, 70, 70)
 
 game = True
+finish = False
 while game:
     window.fill((150, 150, 250))
     player_L.reset()
     player_R.reset()
+    ball.reset()
     player_L.updateL()
     player_R.updateR()
+    ball.update(player_L, player_R)
     for e in event.get():
         if e.type == QUIT:
             game = False
